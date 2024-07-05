9a2c47f622d72b0b5fa20cbb1080ee7ecb4e6e63
Wojciech Mokwiński <mokwinskiwojciech0@gmail.com>
2024-07-05 13:09:00 +0200

    Add logo

--- move-act-contest-platform/src/App.tsx
+++ move-act-contest-platform/src/App.tsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 function App() {
 	return (
 		<div className="container">
 			<h1 className="text-center">Hello, Bootstrap in React!</h1>
-			<button className="">Click Me</button>
+			<button className="btn btn-primary">Click Me</button>
 		</div>
 	);
 }
